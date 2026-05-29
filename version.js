45972affea00e11fe56aed36618fe7a5b966afe4
Panchorn <nonpcn@gmail.com>
2026-05-29 21:52:17 +0700

    fix get tag version

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
-const VERSION = "9f94e08";
-const GIT_HASH = "9f94e08";
-const BUILD_TIME = "2026-05-29 21:23:32";
+const VERSION = "1.0.5";
+const GIT_HASH = "2727e4d";
+const BUILD_TIME = "2026-05-29 21:49:07 ICT";
